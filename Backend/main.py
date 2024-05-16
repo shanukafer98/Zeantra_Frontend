5656331e6d29da0d9755b4b1e157acde0a5c72b0
shanukafer98 <shanukafer98@gmail.com>
2024-05-16 15:40:12 +0530

    chore: Update proxy URL in package.json to use HTTP protocol

--- Backend/main.py
+++ Backend/main.py
@@ -44,15 +44,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Vite app is running on port 5173
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
-    allow_headers=["*"],
-)
-
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", 10000)))
